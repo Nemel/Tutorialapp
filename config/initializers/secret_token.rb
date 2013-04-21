6844c0d539b72297efb155735da88f1686c6c670
Nemel <steffenkube@gmx.de>
2013-04-21 15:03:24 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tutapp2::Application.config.secret_token = '92081ab2599297358f0d8d3066e51484f3f7169fae2d3bc170d209127a3351bb471b8ad7d75bfcd2c074067a3a51229f1ffa541646e0ea935d5ebfd7f238aae0'
